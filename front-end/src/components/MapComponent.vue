8e7673fb56d2bb516605911474f07c59dc58a583
Jacob Anderson <jacobdanderson@gmail.com>
2025-12-05 22:46:59 -0500

    Updating dependencies and fixing minor issues in configuration files

--- front-end/src/components/MapComponent.vue
+++ front-end/src/components/MapComponent.vue
@@ -1,7 +1,14 @@
 <script lang="ts" setup>
-import L from "leaflet";
 import { nextTick, onBeforeUnmount, onMounted, ref, watch } from "vue";
-import "leaflet/dist/leaflet.css";
+
+defineOptions({ ssr: false });
+
+const props = defineProps({
+	coordinates: {
+		type: Array as () => Location[],
+		default: () => []
+	}
+});
 
 interface Location {
 	latitude: number;
@@ -12,55 +19,57 @@ interface Location {
 	imageUrl: string;
 }
 
-const props = defineProps({
-	coordinates: {
-		type: Array as () => Location[],
-		default: () => []
-	}
-});
-
-const map = ref<L.Map | null>(null);
+const map = ref<any>(null);
 const mapContainer = ref<HTMLElement | null>(null);
 
 onMounted(async () => {
+	// Import Leaflet only on client
+	const L = await import("leaflet");
+	await import("leaflet/dist/leaflet.css");
+
 	await nextTick();
-	if (mapContainer.value) {
-		map.value = L.map(mapContainer.value).setView([40.2338, -111.6585], 5);
 
-		L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-			attribution:
-				'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(map.value as L.Map);
+	if (!mapContainer.value) return;
 
-		watch(
-			props.coordinates,
-			(newCoordinates: Location[]) => {
-				newCoordinates.forEach((location: Location) => {
-					const marker = L.marker([
-						location.latitude,
-						location.longitude
-					]).addTo(map.value as L.Map);
-					const popupContent = `
-        <b>${location.name}</b><br>
-        ${location.events.join(", ")}<br>
-        ${location.figures.join(", ")}<br>
-        <img src="${location.imageUrl}" alt="${location.name}" style="width:100%;max-width:300px;">
-      `;
-					marker.bindPopup(popupContent);
-				});
-			},
-			{ immediate: true }
-		);
-	}
+	map.value = L.map(mapContainer.value).setView([40.2338, -111.6585], 5);
+
+	L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+		attribution:
+			'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map.value);
+
+	// Watch markers
+	watch(
+		() => props.coordinates,
+		(newCoordinates: Location[]) => {
+			newCoordinates.forEach((location: Location) => {
+				const marker = L.marker([
+					location.latitude,
+					location.longitude
+				]).addTo(map.value);
+
+				const popupContent = `
+                    <b>${location.name}</b><br>
+                    ${location.events.join(", ")}<br>
+                    ${location.figures.join(", ")}<br>
+                    <img src="${location.imageUrl}" alt="${location.name}" style="width:100%;max-width:300px;">
+                `;
+				marker.bindPopup(popupContent);
+			});
+		},
+		{ immediate: true }
+	);
 });
 
 onBeforeUnmount(() => {
-	if (map.value instanceof L.Map) map.value.remove();
+	if (map.value && map.value.remove) {
+		map.value.remove();
+	}
 });
 </script>
 
 <template>
-	<div id="map" ref="mapContainer" />
+	<div id="map" ref="mapContainer"></div>
 </template>
 
 <style scoped>
