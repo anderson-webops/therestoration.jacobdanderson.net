b5a0c85563a2a4dab157cb8897c8fef705975a42
Jacob Anderson <jacobdanderson@gmail.com>
2025-12-05 23:07:54 -0500

    Refactor password plugin to use HydratedDocument for better type safety

--- back-end/src/models/plugins/password.ts
+++ back-end/src/models/plugins/password.ts
@@ -1,14 +1,14 @@
 // src/models/plugins/password.ts
-import type { Document, Schema } from "mongoose";
+import type { Document, HydratedDocument, Schema } from "mongoose";
 import argon2 from "argon2";
 
-// Make sure T includes both password: string AND Document
-export function passwordPlugin<T extends Document & { password: string }>(schema: Schema<T>) {
-	schema.pre("save", async function (this: T, next) {
-		// 'isModified' comes from Document
-		if (!this.isModified("password")) return next();
+export function passwordPlugin<T extends Document & { password: string }>(
+	schema: Schema<T>
+) {
+	schema.pre("save", async function (this: HydratedDocument<T>) {
+		if (!this.isModified("password")) return;
+
 		this.password = await argon2.hash(this.password);
-		next();
 	});
 
 	schema.methods.comparePassword = function (pw: string) {
