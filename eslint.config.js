f8f34841575f676e7a381f0dc5d644d3e04e8626
YunYouJun <me@yunyoujun.cn>
2026-02-25 22:38:37 +0800

    chore: update dependencies and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -7,4 +7,10 @@ export default antfu(
     formatters: true,
     pnpm: true,
   },
+  {
+    files: ['README.md', 'README.zh-CN.md'],
+    rules: {
+      'markdown/heading-increment': 'off',
+    },
+  },
 )
